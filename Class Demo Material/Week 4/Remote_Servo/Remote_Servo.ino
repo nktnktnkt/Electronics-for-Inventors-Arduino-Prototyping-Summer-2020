87f242384468b3e6effbd043441ef6cd9b46b1c5
Nolan Thomas <nolan.thomas@pcc.edu>
2020-07-28 16:16:03 -0700

    updates

--- Class Demo Material/Week 4/Remote_Servo/Remote_Servo.ino
+++ Class Demo Material/Week 4/Remote_Servo/Remote_Servo.ino
@@ -25,28 +25,28 @@ void loop() {
     Serial.println(results.value, HEX);
     irrecv.resume();
 
-    if(results.value == 0xFF30CF){
-      servoPos = 45;
+    if(results.value == 0xFF30CF){ //1 button
+      servoPos = 45; //servo position in degrees
     }
     
-    if(results.value == 0xFF18E7){
+    if(results.value == 0xFF18E7){ //2 button
       servoPos = 100;
     }
-    if(results.value == 0xFF7A85){
+    if(results.value == 0xFF7A85){ //3 button
       servoPos = 180;
     }
     
-    if(results.value == 0xFF6897){
+    if(results.value == 0xFF6897){ //0 button
       servoPos = 0;
     }
-    if(results.value == 0xFF906F){
+    if(results.value == 0xFF906F){ //+ button
       servoPos += 10;
     }
-    if(results.value == 0xFFA857){
+    if(results.value == 0xFFA857){ //- button
       servoPos -= 10;
     }
     
   }
-  myservo.write(servoPos);
+  myservo.write(servoPos); //can be degrees between 0 and 180
   
 }
